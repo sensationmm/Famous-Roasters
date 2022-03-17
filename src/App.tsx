a0a97d3851be0a6331577ad722a10c86845da6f2
minnocci-bcgdv <juan.minnocci@bcgdv.com>
2022-03-17 22:31:57 +0100

    fix(ci): permissions

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Hello world
+          Hello world!
         </a>
       </header>
     </div>
