046939cfde9a3a94c9c9b131bfc907ead7444fbf
minnocci-bcgdv <juan.minnocci@bcgdv.com>
2022-03-18 11:29:07 +0100

    fix(ci): test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/hello Rob!/i)
+  const linkElement = screen.getByText(/hello Roaster!/i)
   expect(linkElement).toBeInTheDocument()
 })
--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Hello Rob!
+          Hello Roaster!
         </a>
       </header>
     </div>
