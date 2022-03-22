392a488944c071fecd5b9cf7e7e4f6673241e88b
minnocci-bcgdv <juan.minnocci@bcgdv.com>
2022-03-22 15:50:08 +0100

    feat(design system): completes tests

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,7 +1,7 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import React from 'react'
 
-import { Button, Color, Emphasis, Size } from './Button'
+import { Button, Color, Emphasis } from './Button'
 
 export default {
   title: 'Components/Button',
--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,13 +1,73 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import React from 'react'
 
-import { Button } from '.'
+import { Button, ButtonColor,ButtonEmphasis, ButtonSize } from '.'
 
 global.alert = jest.fn()
 
 describe('Button component', () => {
   it('Renders correctly', async () => {
     const { container } = render(<Button onClick={() => alert('click')}>CTA</Button>)
+    await waitFor(() => new Promise((res) => setTimeout(res, 0)))
+    expect(container).toMatchSnapshot()
+  })
+
+  it('Renders correctly secondary', async () => {
+    const { container } = render(
+      <Button emphasis={ButtonEmphasis.Contained} color={ButtonColor.Secondary} onClick={() => alert('click')}>
+        CTA
+      </Button>,
+    )
+    await waitFor(() => new Promise((res) => setTimeout(res, 0)))
+    expect(container).toMatchSnapshot()
+  })
+
+  it('Renders correctly size sm', async () => {
+    const { container } = render(
+      <Button size={ButtonSize.sm} onClick={() => alert('click')}>
+        CTA
+      </Button>,
+    )
+    await waitFor(() => new Promise((res) => setTimeout(res, 0)))
+    expect(container).toMatchSnapshot()
+  })
+
+  it('Renders correctly size lg', async () => {
+    const { container } = render(
+      <Button size={ButtonSize.lg} onClick={() => alert('click')}>
+        CTA
+      </Button>,
+    )
+    await waitFor(() => new Promise((res) => setTimeout(res, 0)))
+    expect(container).toMatchSnapshot()
+  })
+
+  it('Renders correctly outlined', async () => {
+    const { container } = render(
+      <Button emphasis={ButtonEmphasis.Outlined} onClick={() => alert('click')}>
+        CTA
+      </Button>,
+    )
+    await waitFor(() => new Promise((res) => setTimeout(res, 0)))
+    expect(container).toMatchSnapshot()
+  })
+
+  it('Renders correctly text', async () => {
+    const { container } = render(
+      <Button emphasis={ButtonEmphasis.Text} onClick={() => alert('click')}>
+        CTA
+      </Button>,
+    )
+    await waitFor(() => new Promise((res) => setTimeout(res, 0)))
+    expect(container).toMatchSnapshot()
+  })
+
+  it('Renders correctly with extra classes', async () => {
+    const { container } = render(
+      <Button className="anotherClass" onClick={() => alert('click')}>
+        CTA
+      </Button>,
+    )
     await waitFor(() => new Promise((res) => setTimeout(res, 0)))
     expect(container).toMatchSnapshot()
   })
