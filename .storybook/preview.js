05b5acda7d11a2d0786423f4e644b27deac320e0
minnocci-bcgdv <juan.minnocci@bcgdv.com>
2022-03-23 11:49:46 +0100

    feat(scaffolding): fix storybook router

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,9 @@
 import '../src/index.css'
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+import { addDecorator } from "@storybook/react";
+import { MemoryRouter } from "react-router";
+
+addDecorator(story => <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
