873f3679d1d38492b21dfcc0620962534e484de3
minnocci-bcgdv <juan.minnocci@bcgdv.com>
2022-03-28 14:34:47 +0200

    feat(catalogue): coverage

--- src/config/apollo/index.tsx
+++ src/config/apollo/index.tsx
@@ -1,20 +1,9 @@
 import { ApolloClient, ApolloLink, createHttpLink, from, InMemoryCache, NormalizedCacheObject } from '@apollo/client'
-import { onError } from '@apollo/client/link/error'
 
 export const apolloClient = (): ApolloClient<NormalizedCacheObject> => {
-  const errorLink = onError(({ graphQLErrors, networkError }) => {
-    if (graphQLErrors)
-      graphQLErrors.forEach(({ message: errorMessage, locations, path }) => {
-        console.log(`[GraphQL error]: Message: ${errorMessage}, Location: ${locations}, Path: ${path}`)
-      })
-
-    if (networkError) console.log(`[Network error]: ${networkError}`)
-  })
-
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: from([
-      errorLink,
       createHttpLink({
         uri: process.env.REACT_APP_SHOPIFY_STOREFRONT_GRAPHQL_ENDPOINT,
         headers: {
