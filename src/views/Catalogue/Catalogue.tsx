54a988b70ffaa46365f61896ff1a6098b747b837
minnocci-bcgdv <juan.minnocci@bcgdv.com>
2022-04-04 16:55:25 +0200

    feat(catalogue): removes code smell

--- src/views/Catalogue/Catalogue.tsx
+++ src/views/Catalogue/Catalogue.tsx
@@ -17,22 +17,28 @@ export const Catalogue: React.FC = () => {
   const edges = data?.products.edges
   const productNodes = edges?.map((edge) => edge.node)
 
+  const renderProducts = () => {
+    if (loading) {
+      return <Loader />
+    } else {
+      if (error) {
+        return <ErrorPrompt promptAction={() => history.go(0)} />
+      } else {
+        return (
+          <div className="grid gap-2 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+            {productNodes?.map((node, i: number) => (
+              <ProductTile key={`title-${i}`} productNode={node} />
+            ))}
+          </div>
+        )
+      }
+    }
+  }
+
   return (
     <Layout>
       <main className="flex flex-grow w-full items-start justify-center bg-white mt-4">
-        <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 xl:px-8">
-          {loading ? (
-            <Loader />
-          ) : error ? (
-            <ErrorPrompt promptAction={() => history.go(0)} />
-          ) : (
-            <div className="grid gap-2 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-              {productNodes?.map((node, i: number) => (
-                <ProductTile key={`title-${i}`} productNode={node} />
-              ))}
-            </div>
-          )}
-        </div>
+        <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 xl:px-8">{renderProducts()}</div>
       </main>
     </Layout>
   )
