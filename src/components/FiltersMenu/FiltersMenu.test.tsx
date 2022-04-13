a0077987decdc66383d7fb4ac3c5ca1703453109
minnocci-bcgdv <juan.minnocci@bcgdv.com>
2022-04-13 12:25:07 +0200

    feat(catalogue): adds filters - filter menu structure

--- src/components/FiltersMenu/FiltersMenu.test.tsx
+++ src/components/FiltersMenu/FiltersMenu.test.tsx
@@ -1,6 +1,7 @@
-import { render, waitFor } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import React from 'react'
 import { I18nextProvider } from 'react-i18next'
+import { MemoryRouter } from 'react-router-dom'
 import { i18n } from 'src/config'
 
 import { FiltersMenu } from '.'
@@ -16,5 +17,24 @@ describe('Filters Menu component', () => {
     )
     await waitFor(() => new Promise((res) => setTimeout(res, 0)))
     expect(container).toMatchSnapshot()
+  })
+
+  it('Can expand, collapse and click on remove', async () => {
+    render(
+      <I18nextProvider i18n={i18n}>
+        <MemoryRouter initialEntries={['/catalogue']}>
+          <FiltersMenu />
+        </MemoryRouter>
+      </I18nextProvider>,
+    )
+    const buttonOpen = await screen.findByTestId('button-filters-menu-open')
+    expect(buttonOpen).toBeInTheDocument()
+    fireEvent.click(buttonOpen)
+    const buttonRemove = await screen.findByTestId('button-filters-menu-remove')
+    expect(buttonRemove).toBeInTheDocument()
+    fireEvent.click(buttonRemove)
+    const buttonClose = await screen.findByTestId('button-filters-menu-close')
+    expect(buttonClose).toBeInTheDocument()
+    fireEvent.click(buttonClose)
   })
 })
--- src/components/FiltersMenu/FiltersMenu.tsx
+++ src/components/FiltersMenu/FiltersMenu.tsx
@@ -1,16 +1,99 @@
-import React from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { TrashIcon } from '@heroicons/react/outline'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
+import React, { Fragment, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import { Typography, TypographySize } from 'src/components'
 
+interface FiltersData {
+  key: string
+}
+
+const filtersData: FiltersData[] = [
+  { key: 'pricePerKg' },
+  { key: 'beanType' },
+  { key: 'roaster' },
+  { key: 'origin' },
+  { key: 'packageSize' },
+]
+
 export const FiltersMenu: React.FC = () => {
+  const [open, setOpen] = useState(false)
+  const { t } = useTranslation()
+
   return (
-    <button
-      className="inline-flex justify-between w-full px-4 py-2 text-left bg-white rounded-full border border-coreUI-text-tertiary cursor-default"
-      data-testid="button-filters-menu"
-      onClick={() => alert('Hello world!')}
-    >
-      <Typography size={TypographySize.Small} className="block truncate">
-        Filter
-      </Typography>
-    </button>
+    <>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="fixed inset-0 flex z-40 xl:hidden" onClose={setOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="transition-opacity ease-linear duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity ease-linear duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <Transition.Child
+            as={Fragment}
+            enter="transition ease-in-out duration-300 transform"
+            enterFrom="-translate-x-full"
+            enterTo="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leaveFrom="translate-x-0"
+            leaveTo="-translate-x-full"
+          >
+            <div className="relative w-full bg-white shadow-xl pb-12 flex flex-col overflow-y-auto">
+              <div className="px-5 pt-5 pb-5 flex justify-between">
+                <button
+                  type="button"
+                  className="-m-2 p-2 rounded-md inline-flex items-center justify-center text-gray-400"
+                  onClick={() => setOpen(false)}
+                  data-testid="button-filters-menu-close"
+                >
+                  <span className="sr-only">{t(`pages.catalogue.filters.common.filtersMenu.close`)}</span>
+                  <ChevronLeftIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+                <Typography>{t(`pages.catalogue.filters.common.filtersMenu.filterOptions`)}</Typography>
+                <button
+                  type="button"
+                  className="-m-2 p-2 rounded-md inline-flex items-center justify-center text-gray-400"
+                  onClick={() => alert('remove')}
+                  data-testid="button-filters-menu-remove"
+                >
+                  <span className="sr-only">{t(`pages.catalogue.filters.common.filtersMenu.removeFilter`)}</span>
+                  <TrashIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+
+              <div className="border-t border-coreUI-text-tertiary">
+                {filtersData.map((filter) => (
+                  <div
+                    key={`filter-${filter.key}`}
+                    className="flex justify-between px-5 py-5 border-b border-coreUI-text-tertiary"
+                  >
+                    <Typography className="inline-flex">{t(`pages.catalogue.filters.${filter.key}.label`)}</Typography>
+                    <ChevronRightIcon className="inline-flex h-6 w-6" aria-hidden="true" />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </Transition.Child>
+        </Dialog>
+      </Transition.Root>
+      <button
+        className="inline-flex justify-between w-full px-4 py-2 text-left bg-white rounded-full border border-coreUI-text-tertiary cursor-default md:hidden"
+        data-testid="button-filters-menu-open"
+        onClick={() => setOpen(true)}
+      >
+        <Typography size={TypographySize.Small} className="block truncate">
+          {t(`pages.catalogue.filters.common.filtersMenu.filter`)}
+        </Typography>
+        <ChevronRightIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+      </button>
+    </>
   )
 }
