17d2c8ccb784940c882f876338ff96dbd1dac211
minnocci-bcgdv <juan.minnocci@bcgdv.com>
2022-05-05 15:30:52 +0200

    feat(catalogue): fixes code smells

--- src/components/FiltersMenuMobile/FiltersMenuMobile.tsx
+++ src/components/FiltersMenuMobile/FiltersMenuMobile.tsx
@@ -131,11 +131,10 @@ export const FiltersMenuMobile: React.FC<FiltersProps> = ({ onUpdateFilters, ini
                         <CheckIcon className="w-5 h-5 mr-2 text-brand-green-club" aria-hidden="true" />
                       )}
                       <Typography className="inline-flex">
-                        {filter?.filterValuesSelected && filter.filterValuesSelected.length > 0
-                          ? `${t(`pages.catalogue.filters.${filter.key}.label`)} (${
-                              filter.filterValuesSelected.length
-                            })`
-                          : t(`pages.catalogue.filters.${filter.key}.label`)}
+                        {t(`pages.catalogue.filters.${filter.key}.label`)}
+                        {filter?.filterValuesSelected &&
+                          filter.filterValuesSelected.length > 0 &&
+                          ` (${filter.filterValuesSelected.length})`}
                       </Typography>
                     </div>
                     <ChevronRightIcon className="inline-flex h-6 w-6" aria-hidden="true" />
@@ -166,9 +165,8 @@ export const FiltersMenuMobile: React.FC<FiltersProps> = ({ onUpdateFilters, ini
         onClick={() => setOpen(true)}
       >
         <Typography size={TypographySize.Small} className="block truncate">
-          {filtersApplied > 0
-            ? `${t(`pages.catalogue.filters.common.filtersMenu.filter`)} (${filtersApplied})`
-            : t(`pages.catalogue.filters.common.filtersMenu.filter`)}
+          {t(`pages.catalogue.filters.common.filtersMenu.filter`)}
+          {filtersApplied > 0 && ` (${filtersApplied})`}
         </Typography>
         <ChevronRightIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
       </button>
