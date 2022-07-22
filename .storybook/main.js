bbbe2b9a08ea69d68c182a84de0c015f211c261a
Kevin Reynolds <kevin.reynolds@somoglobal.com>
2022-07-22 12:01:45 -0700

    feat(icon): improve icon managability with defaults

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require('path')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "stories": ['*.stories.mdx',"../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app", 'storybook-react-i18next'],
   "features": {
     "emotionAlias": false,
