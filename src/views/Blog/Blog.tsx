f2d3ee6bb25fbe6747974e7fdf7a3461c3a2ceb1
Bharat Darji <bharat.darji@external.sinnerschrader.com>
2022-08-09 09:47:43 +0200

    chore(blog): remove unused import

--- src/views/Blog/Blog.tsx
+++ src/views/Blog/Blog.tsx
@@ -2,7 +2,6 @@ import { useQuery } from '@apollo/client/react/hooks'
 import { loader } from 'graphql.macro'
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import { useTranslation } from 'react-i18next'
 import { useParams } from 'react-router-dom'
 import {
   ErrorPrompt,
