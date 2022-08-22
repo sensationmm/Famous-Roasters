6f6acc331607797902f31b9f2601e44c638c9c80
Kevin Reynolds <kevin.reynolds@somoglobal.com>
2022-08-22 08:51:32 -0700

    feat(catalogue): update collection ids

--- src/config/shopify/index.ts
+++ src/config/shopify/index.ts
@@ -1,2 +1,2 @@
-export const shopifyCoffeeCollection = 'gid://shopify/Collection/386655387864'
-export const shopifyAccessoryCollection = 'gid://shopify/Collection/479115477258'
+export const shopifyCoffeeCollection = 'gid://shopify/Collection/270869135443'
+export const shopifyAccessoryCollection = 'gid://shopify/Collection/270869364819'
