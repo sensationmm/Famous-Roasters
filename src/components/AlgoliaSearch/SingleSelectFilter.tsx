4cf5bfd55644b9d2696c465fea5d6c8964088c8f
Hannes Hesse <hannes.hesse@bcgdv.com>
2022-08-24 10:35:16 +0200

    feat(search): fix behavior of single select filter

--- src/components/AlgoliaSearch/SingleSelectFilter.tsx
+++ src/components/AlgoliaSearch/SingleSelectFilter.tsx
@@ -1,5 +1,5 @@
 import { ChevronDownIcon } from '@heroicons/react/outline'
-import { useRefinementList, UseRefinementListProps } from 'react-instantsearch-hooks-web'
+import { useClearRefinements, useRefinementList, UseRefinementListProps } from 'react-instantsearch-hooks-web'
 
 interface FilterProps extends UseRefinementListProps {
   defaultText: string
@@ -7,22 +7,26 @@ interface FilterProps extends UseRefinementListProps {
 
 const SingleSelectFilter = (props: FilterProps) => {
   const { items, refine } = useRefinementList({ sortBy: ['name:asc'], ...props })
+  const { refine: clearRefinements } = useClearRefinements()
   const { defaultText } = props
 
   return (
     <span>
       <select
         onChange={(e) => {
+          clearRefinements()
           refine(e.target.value)
         }}
         className="nochevron text-xl leading-7 font-semibold border-b-2 minimal pb-2 pr-7"
       >
         <option value="">{defaultText}</option>
         {items.map((item) => (
-          <option key={item.value}>{item.value}</option>
+          <option key={item.value} selected={item.isRefined}>
+            {item.value}
+          </option>
         ))}
       </select>
-      <ChevronDownIcon className="-ml-5 h-6 w-6 inline" aria-hidden="true" />
+      <ChevronDownIcon className="-ml-5 -mt-1 h-6 w-6 inline" aria-hidden="true" />
     </span>
   )
 }
