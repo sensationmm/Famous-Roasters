f41cc6a3d8c178dbeb6bed41027a0fa01b48df67
Kevin Reynolds <kevin@sensationmultimedia.co.uk>
2022-09-10 02:53:58 -0700

    fix(catalogue): main filter clearing others (#263)
    * fix(catalogue): main filter clearing others
    
    * fix(catalogue): fix snapshot failures caused by missing local package
    
    Co-authored-by: Kevin Reynolds <kevin@sensarionmultimedia.co.uk>

--- src/components/AlgoliaSearch/SingleSelectFilter.tsx
+++ src/components/AlgoliaSearch/SingleSelectFilter.tsx
@@ -1,4 +1,4 @@
-import { useClearRefinements, useRefinementList, UseRefinementListProps } from 'react-instantsearch-hooks-web'
+import { useRefinementList, UseRefinementListProps } from 'react-instantsearch-hooks-web'
 
 import SingleSelect, { SelectItem } from './SingleSelect'
 
@@ -10,14 +10,13 @@ interface FilterProps extends UseRefinementListProps {
 const SingleSelectFilter = (props: FilterProps) => {
   const { defaultText, big } = props
   const { items, refine } = useRefinementList({ sortBy: ['name:asc'], ...props })
-  const { refine: clearRefinements } = useClearRefinements()
 
   const defaultItem = { label: defaultText, value: '', isRefined: false }
   const options = [defaultItem].concat(items.filter((item) => item.value.length > 0))
   const currentItem: SelectItem = options.filter((item) => item.isRefined)[0] || defaultItem
 
   const handleOnChange = (option: SelectItem) => {
-    clearRefinements()
+    refine(currentItem.value)
     refine(option.value)
   }
 
