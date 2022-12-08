5cb8ab76c3865b3ed2dbe3d05b8033ef7a90a1e9
Kevin Reynolds <kevin@sensarionmultimedia.co.uk>
2022-12-08 15:41:42 -0800

    fix(catalog): sonarcloud failure

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,12 @@
 import { toRoundedValueInRealScale, toValueInHumanScale } from './attributeScaleUtils'
 import { formatDate, formatPrice, getAPIId, getAromaKey, getSimplifiedId, normalizeString } from './formatters'
-import { formatBlogHtmlElement, formatHtmlElement, isAllowedHtmlElement, parseHtmlSafely } from './htmlContentParser'
+import {
+  formatBlogHtmlElement,
+  formatCategoryHtmlElement,
+  formatHtmlElement,
+  isAllowedHtmlElement,
+  parseHtmlSafely,
+} from './htmlContentParser'
 import { useLocalStorage } from './localStorage'
 import { readTimeCalculator } from './readTimeCalculator'
 
@@ -12,6 +18,7 @@ export { getSimplifiedId as getSimplifiedId }
 export { useLocalStorage as useLocalStorage }
 export { parseHtmlSafely as parseHtmlSafely }
 export { formatBlogHtmlElement as formatBlogHtmlElement }
+export { formatCategoryHtmlElement }
 export { formatHtmlElement as formatHtmlElement }
 export { isAllowedHtmlElement as isAllowedHtmlElement }
 export { toValueInHumanScale as toValueInHumanScale }
